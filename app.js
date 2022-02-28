bfdef3e3fdca738b252990a212c6ea8c8e16ed7e
Teddy Walter <teddywalter2016@gmail.com>
2022-02-28 17:14:35 +0200

    return html content usign readFileSync

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 console.log("Express tutorial by Teddy Walter");
 const http = require("http");
+const { readFileSync } = require("fs");
+
+/*
+    get all pages content :: we are using synchrome because this bloc
+    is not calling when we request a ressource on the server but is calling
+    when we initialize the server
+*/
+const homePage = readFileSync("./ui/index.html");
 
 http
   .createServer((req, res) => {
@@ -8,7 +16,8 @@ http
     const { url } = req;
     if (url === "/") {
       res.writeHead(200, { "content-type": "text/html" });
-      res.write("<h1>HOME PAGE</h1>");
+      //   res.write("<h1>HOME PAGE</h1>");
+      res.write(homePage);
       res.end();
     } else if (url === "/about") {
       res.writeHead(200, { "content-type": "text/html" });
