aab2d81ce9da8adf349a721235ae7a9e507d01a3
Teddy Walter <teddywalter2016@gmail.com>
2022-03-06 22:13:27 +0200

    static file express

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log("Express Now !!");
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.use(express.static("./public"));
+
+app.get("/", (req, res) => {
+  res.status(200).sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
+  //   console.log();
+});
+
+app.all("*", (req, res) => {
+  res.status(404).send("Oops! Ressouce not found");
+});
+
+app.listen(5000, () => {
+  console.log("Server listen on 5000 ...");
+});
