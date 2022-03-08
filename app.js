3a3f32da964ab5f8490717efe9ce4653205b01ad
Teddy Walter <teddywalter2016@gmail.com>
2022-03-08 14:08:53 +0200

    route

--- app.js
+++ app.js
@@ -1,20 +1,13 @@
 const express = require("express");
 const app = express();
 const peopleRouter = require("./routes/people");
+const authRouter = require("./routes/auth");
 
 app.use(express.static("./methods-public")); //static asseet
 app.use(express.urlencoded({ extended: false })); //to handler form data
 app.use(express.json()); // to handler json data
 app.use("/api/people", peopleRouter);
-
-app.post("/login", (req, res) => {
-  console.log(req.body);
-  const { name } = req.body;
-  if (name) {
-    return res.status(200).send(`Welcome ${name}`);
-  }
-  return res.status(401).send("Please provide credentials");
-});
+app.use("/login", authRouter);
 
 app.listen(5000, () => {
   console.log("Server is listening on port 5000...");
