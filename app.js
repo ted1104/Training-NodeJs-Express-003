bc58e00251219178575c3ace1eb7a5993e072604
Teddy Walter <teddywalter2016@gmail.com>
2022-03-08 14:03:52 +0200

    routes folder

--- app.js
+++ app.js
@@ -1,88 +1,11 @@
 const express = require("express");
 const app = express();
-const { people } = require("./data");
+const peopleRouter = require("./routes/people");
 
-app.use(express.static("./methods-public"));
+app.use(express.static("./methods-public")); //static asseet
 app.use(express.urlencoded({ extended: false })); //to handler form data
 app.use(express.json()); // to handler json data
-
-app.get("/api/people", (req, res) => {
-  res.status(200).json({
-    success: true,
-    data: people,
-  });
-});
-
-app.post("/api/people", (req, res) => {
-  console.log(req.body);
-  const { name } = req.body;
-  if (!name) {
-    return res.status(400).json({
-      success: false,
-      msg: "please provide name value",
-    });
-  }
-  return res.status(201).json({
-    success: true,
-    person: name,
-  });
-});
-
-app.post("/api/postman/people", (req, res) => {
-  const { name } = req.body;
-  if (!name) {
-    return res.status(400).json({
-      success: false,
-      message: "please provide name value",
-    });
-  }
-
-  return res.status(201).json({
-    success: true,
-    data: [...people, name],
-  });
-});
-
-app.put("/api/people/:id", (req, res) => {
-  const { id } = req.params;
-  const { name } = req.body;
-
-  const person = people.find((p) => p.id === Number(id));
-  if (!person) {
-    return res.status(400).json({
-      success: false,
-      msg: "no person with id",
-    });
-  }
-  const newPeople = people.map((person) => {
-    if (person.id === Number(id)) {
-      person.name = name;
-    }
-    return people;
-  });
-
-  return res.status(200).json({
-    success: true,
-    data: newPeople,
-  });
-});
-
-app.delete("/api/people/:id", (req, res) => {
-  const { id } = req.params;
-  const person = people.find((p) => p.id === Number(id));
-  if (!person) {
-    return res.status(400).json({
-      success: false,
-      msg: "no person with id",
-    });
-  }
-
-  const newPeople = people.filter((person) => person.id !== Number(id));
-  return res.status(200).json({
-    success: true,
-    data: newPeople,
-  });
-});
+app.use("/api/people", peopleRouter);
 
 app.post("/login", (req, res) => {
   console.log(req.body);
---
+++ routes/people.js
@@ -0,0 +1,83 @@
+const express = require("express");
+const router = express.Router();
+const { people } = require("../data");
+
+router.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    data: people,
+  });
+});
+
+router.post("/", (req, res) => {
+  console.log(req.body);
+  const { name } = req.body;
+  if (!name) {
+    return res.status(400).json({
+      success: false,
+      msg: "please provide name value",
+    });
+  }
+  return res.status(201).json({
+    success: true,
+    person: name,
+  });
+});
+
+router.post("/postman", (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    return res.status(400).json({
+      success: false,
+      message: "please provide name value",
+    });
+  }
+
+  return res.status(201).json({
+    success: true,
+    data: [...people, name],
+  });
+});
+
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  const person = people.find((p) => p.id === Number(id));
+  if (!person) {
+    return res.status(400).json({
+      success: false,
+      msg: "no person with id",
+    });
+  }
+  const newPeople = people.map((person) => {
+    if (person.id === Number(id)) {
+      person.name = name;
+    }
+    return people;
+  });
+
+  return res.status(200).json({
+    success: true,
+    data: newPeople,
+  });
+});
+
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  const person = people.find((p) => p.id === Number(id));
+  if (!person) {
+    return res.status(400).json({
+      success: false,
+      msg: "no person with id",
+    });
+  }
+
+  const newPeople = people.filter((person) => person.id !== Number(id));
+  return res.status(200).json({
+    success: true,
+    data: newPeople,
+  });
+});
+
+module.exports = router;
